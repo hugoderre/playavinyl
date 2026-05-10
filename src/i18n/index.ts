39b825bd14f5f0901fe33d941899830264dfe639
Hugo DERRE <>
2026-05-10 22:56:01 +0200

    feat(i18n): set English as default and fallback language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,11 +5,11 @@ import en from './locales/en.json'
 
 const browserLng = navigator.language.split('-')[0]
 const supportedLngs = ['fr', 'en']
-const lng = supportedLngs.includes(browserLng) ? browserLng : 'fr'
+const lng = supportedLngs.includes(browserLng) ? browserLng : 'en'
 
 i18n.use(initReactI18next).init({
   lng,
-  fallbackLng: 'fr',
+  fallbackLng: 'en',
   resources: {
     fr: { translation: fr },
     en: { translation: en },
